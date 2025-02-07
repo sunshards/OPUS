243e7f7c8116a957149cac1be79be44253f79950
simoneboscaglia <simone.boscaglia@gmail.com>
2025-02-07 23:22:06 +0100

    Initial Commit

---
+++ HeartLogger Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeartLogger Watch App
+//
+//  Created by Simone Boscaglia on 07/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeartLogger Watch App/HeartLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartLoggerApp.swift
+//  HeartLogger Watch App
+//
+//  Created by Simone Boscaglia on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct HeartLogger_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
